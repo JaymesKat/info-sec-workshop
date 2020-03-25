3424c82d313612f3bc5834102c0b724c71ea1fcf
James Katarikawe <jpkat92@gmail.com>
2020-03-25 16:27:25 +0300

    add helmetjs

--- myApp.js
+++ myApp.js
@@ -16,7 +16,7 @@ var app = express();              // Do Not Edit
 // [Helmet](https://github.com/helmetjs/helmet) helps you secure your
 // Express apps by setting various HTTP headers.
 // Install the package, then require it.
-
+var helmet = require('helmet');
 
 
 /** 2) Hide potentially dangerous information - `helmet.hidePoweredBy()` */
