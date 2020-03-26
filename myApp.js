7218c45d388fa423c789f29a285e96d50d286483
James Katarikawe <jpkat92@gmail.com>
2020-03-26 23:08:14 +0300

    Add middleware to mitigate clickjacking

--- myApp.js
+++ myApp.js
@@ -45,7 +45,7 @@ app.use(helmet.hidePoweredBy());
 
 // We don't need our app to be framed, so you should use `helmet.frameguard()`
 // passing to it the configuration object `{action: 'deny'}`
-
+app.use(helmet.frameguard({action: 'deny'}));
  
 
 /** 4) Mitigate the risk of XSS - `helmet.xssFilter()` */
