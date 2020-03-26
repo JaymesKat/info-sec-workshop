e695f71081bc216e87c434a29e351678c9aa6faa
James Katarikawe <jpkat92@gmail.com>
2020-03-26 22:59:10 +0300

    Hide Express-specific response header

--- myApp.js
+++ myApp.js
@@ -30,7 +30,7 @@ var helmet = require('helmet');
 // people off. e.g. `helmet.hidePoweredBy({ setTo: 'PHP 4.2.0' })`
 
 // Use `helmet.hidePoweredBy()``
-
+app.use(helmet.hidePoweredBy());
 
 
 /** 3) Mitigate the risk of clickjacking - `helmet.frameguard()` */
